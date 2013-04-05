bf7d5d95a67a772b9b367f06be486b8215bf34f4
liquid0624 <rjbirnesser@gmail.com>
2013-04-05 18:52:44 -0400

    Use Global Dalvik Heapsizes

--- device.mk
+++ device.mk
@@ -173,6 +173,8 @@ PRODUCT_PACKAGES += \
 	e2fsck \
 	setup_fs
 
+$(call inherit-product, frameworks/native/build/phone-xhdpi-1024-dalvik-heap.mk)
+
 $(call inherit-product-if-exists, vendor/nxp/pn544/nxp-pn544-fw-vendor.mk)
 $(call inherit-product, hardware/ti/omap4xxx/omap4.mk)
 $(call inherit-product-if-exists, vendor/ti/proprietary/omap4/ti-omap4-vendor.mk)
