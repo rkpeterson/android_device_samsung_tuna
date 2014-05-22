c4ed156b6f63a6f3075b724f4c1a208c8b20c354
osm0sis <osm0sis@outlook.com>
2014-05-22 17:08:39 -0300

    ramdisk: sync updates for DirtyV and Synapse
    Conflicts:
    	device.mk
    	dvbootscript.sh
    	synapse/actions/bracket-option
    	synapse/actions/color
    	synapse/actions/generic
    	synapse/actions/live
    	synapse/actions/voltage
    	synapse/actions/zram
    	synapse/config.json.generate
    	synapse/config.json.generate.cpu
    	synapse/config.json.generate.gpu-panel
    	synapse/config.json.generate.io
    	synapse/config.json.generate.misc
    	synapse/config.json.generate.voltage
    	uci

--- device.mk
+++ device.mk
@@ -70,6 +70,11 @@ PRODUCT_COPY_FILES += \
 	device/samsung/tuna/init.tuna.usb.rc:root/init.tuna.usb.rc \
 	device/samsung/tuna/fstab.tuna:root/fstab.tuna \
 	device/samsung/tuna/ueventd.tuna.rc:root/ueventd.tuna.rc \
+	device/samsung/tuna/dvbootscript.sh:root/sbin/dvbootscript.sh \
+	device/samsung/tuna/dvmediarevert:root/sbin/dvmediarevert \
+	device/samsung/tuna/hostapd:root/sbin/hostapd \
+	device/samsung/tuna/suspend_trim:root/sbin/suspend_trim \
+	device/samsung/tuna/media_profiles.xml.sr:root/sbin/media_profiles.xml \
 	device/samsung/tuna/media_profiles.xml:system/etc/media_profiles.xml \
 	device/samsung/tuna/media_codecs.xml:system/etc/media_codecs.xml \
 	device/samsung/tuna/gps.conf:system/etc/gps.conf
